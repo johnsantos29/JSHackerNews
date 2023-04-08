53f4a683fd556822ed6500e0639351cdb1e138c0
John Erick Santos <johnsantosdev@gmail.com>
2023-04-09 06:19:50 +1000

    Initial Commit

---
+++ JSHackerNewsApp/JSHackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSHackerNewsAppApp.swift
+//  JSHackerNewsApp
+//
+//  Created by John Erick Santos on 9/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct JSHackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
